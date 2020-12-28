371411e3fd6bbbdf45cd8f363f42f79885dcebf7
Oz Ramos <midiblocks@gmail.com>
2020-12-27 16:53:46 -0800

    #1 Get permission to start webcam and persist it (or show error)

--- src/background/handsfree.js
+++ src/background/handsfree.js
@@ -2,5 +2,9 @@
  * Open the Options Page to prompt to capture the webcam feed
  */
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.runtime.openOptionsPage()
+  chrome.storage.local.get(['hasCapturedStream'], (data) => {
+    if (!data.hasCapturedStream) {
+      chrome.runtime.openOptionsPage()
+    }
+  })
 })
